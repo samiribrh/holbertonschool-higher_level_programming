43e80456b182dc7071549a8946da4ba708976470
samiribrh <samiribrahimov2277@gmail.com>
2024-05-16 23:48:06 +0400

    format correction

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for i in range(0, 100):
     if (i == 99):
         print("{}".format(i))
-    else:
-        print("{}".format(i), end = ", ")
+    elif i < 10:
+        print("{:02d}".format(i), end = ", ")
