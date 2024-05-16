ed13887d00f62d9047f8b7fcb9f23384586b7c86
samiribrh <samiribrahimov2277@gmail.com>
2024-05-16 23:06:05 +0400

    typo correction

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for ch in str:
         if 96 < ord(ch) < 123:
             ch = chr(ord(ch) - 32)
-        print("{}",format(ch), end='')
+        print("{}".format(ch), end='')
     print()
