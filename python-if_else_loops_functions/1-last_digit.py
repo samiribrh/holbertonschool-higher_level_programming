02fa4b1b18e123dee241d09501b3d7ca04956976
samiribrh <samiribrahimov2277@gmail.com>
2024-05-16 22:29:11 +0400

    -last_digit case

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 if number < 0:
-    last_digit = 10 - last_digit
+    last_digit = -(10 - last_digit)
 if last_digit > 5:
     str1 = "greater than 5"
 elif last_digit == 0:
