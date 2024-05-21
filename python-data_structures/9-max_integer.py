f57206165acc45f3a7f52a78dbb076dfef1e6413
samiribrh <samiribrahimov2277@gmail.com>
2024-05-21 10:11:45 +0400

    list max finding

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = my_list[0]
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
