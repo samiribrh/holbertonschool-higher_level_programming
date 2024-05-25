cf06b539be40654245d07640310c80a91909d292
samiribrh <samiribrahimov2277@gmail.com>
2024-05-25 19:26:58 +0400

    generalization removed

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as ve:
+    except ValueError as ve:
         print("Exception: {}".format(ve))
         return False
     else:
