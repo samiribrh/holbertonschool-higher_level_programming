804bb33e3672e8b134483526f91573226b57c939
samiribrh <samiribrahimov2277@gmail.com>
2024-05-25 18:59:14 +0400

    General exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except [TypeError, ValueError]:
+    except Exception:
         return False
     else:
         return True
