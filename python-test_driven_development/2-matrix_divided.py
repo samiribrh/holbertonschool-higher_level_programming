323aefe1c90c0d91e41ceb6413014a29af508413
samiribrh <samiribrahimov2277@gmail.com>
2024-05-27 23:42:14 +0400

    change in foul cases

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,15 +6,12 @@ def matrix_divided(matrix, div):
     """Function for dividing all the elements of matrix"""
 
     # Checking if matrix is a list and its elements is lists containing ints
-    if not (isinstance(matrix, list) and isinstance(i, list) for i in matrix
-            and ((isinstance(a, int) for a in i) for i in matrix)):
+    if not all(isinstance(matrix, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     # Checking if all rows containing same number of elements
-    length = len(matrix)
-    for i in range(length - 1):
-        if len(matrix[i]) != len(matrix[i + 1]):
-            raise TypeError("Each row of the matrix must have the same size")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
     # Checking if div is not a float or int
     if not isinstance(div, (int, float)):
