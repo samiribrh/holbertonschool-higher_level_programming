bd8729eee34d30eaa4154025b9ab4cb32d838df6
samiribrh <samiribrahimov2277@gmail.com>
2024-05-27 21:12:01 +0400

    Change in function and addition to test case

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,18 @@
 
 def add_integer(a, b=98):
     """Adding integers function"""
-    if not (isinstance(a, int) or isinstance(a, float)):
+
+    # Checking if a and b are float or int
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not (isinstance(b, int) or isinstance(b, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+
+    # Casting to int if a and b is floating
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    # Return sum of a and b
+    return a + b
