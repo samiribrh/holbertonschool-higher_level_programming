7ed3a134cf2e726b9e4408b4071cc39b143047c0
samiribrh <samiribrahimov2277@gmail.com>
2024-05-28 17:40:10 +0400

    edge case

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -48,4 +48,6 @@ class Rectangle:
 
     def perimeter(self):
         """Method to return perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
         return 2 * (self.__height + self.__width)
