2f7af3205f14f4f20d6a2b19019cc631e84e948a
samiribrh <samiribrahimov2277@gmail.com>
2024-05-28 18:26:42 +0400

    main removal

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -96,12 +96,3 @@ class Rectangle:
             return rect_2
         else:
             return rect_1
-
-
-my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 3)
-
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
