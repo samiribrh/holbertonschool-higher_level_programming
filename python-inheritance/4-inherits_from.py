12a071e1a7d4914994872cab73760b782bcc5695
samiribrh <samiribrahimov2277@gmail.com>
2024-05-30 17:29:54 +0400

    change to is not

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """Function to check if the object is an instance of a class
     that inherited from the specified class"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
