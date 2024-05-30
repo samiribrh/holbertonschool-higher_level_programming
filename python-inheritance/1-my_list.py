dbc86d06e142480879d5ccbf4148e01f1803b766
samiribrh <samiribrahimov2277@gmail.com>
2024-05-30 17:00:51 +0400

    test cases last

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,3 +9,11 @@ class MyList(list):
         """Method to print sorted list"""
         new_list = sorted(self)
         print(new_list)
+        return new_list
+
+
+my_list = MyList()
+my_list.append("salam")
+my_list.append("1")
+new_list = my_list.print_sorted()
+print(new_list)
