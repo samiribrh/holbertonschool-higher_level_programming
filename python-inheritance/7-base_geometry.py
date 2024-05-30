f3a5cfa706e63af1b8f324386bbe00d2ef167684
samiribrh <samiribrahimov2277@gmail.com>
2024-05-30 18:24:42 +0400

    changed message

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -18,4 +18,4 @@ class BaseGeometry:
 
         # Checking if value is not less or equal to 0
         if value <= 0:
-            raise ValueError("{} must be an integer".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
