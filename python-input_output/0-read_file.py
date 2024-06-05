fae934e3c7a0749fc3fcb4566006c7ca73779fe1
samiribrh <samiribrahimov2277@gmail.com>
2024-06-05 13:58:14 +0400

    read print end

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     # Opening and reading the file
     with open(filename, 'r', encoding='UTF-8') as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
