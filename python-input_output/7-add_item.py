8d1b0f852869897a65aae0e3b648f02897a65061
samiribrh <samiribrahimov2277@gmail.com>
2024-06-06 20:24:35 +0400

    PEP8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Module for adding arguments to a Python list"""
+from sys import argv
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
-import json
-from sys import argv
 
 try:
     new_list = load_from_json_file(filename)
