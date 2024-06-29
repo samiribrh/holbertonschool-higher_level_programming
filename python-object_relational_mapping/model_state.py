d8763cc59a0be4067e40dc01342e85be7542f409
samiribrh <samiribrahimov2277@gmail.com>
2024-06-29 22:50:35 +0400

    State Inherited from Base class

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing State class"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    "The State class"
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
