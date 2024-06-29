2e862f6672dddd8acfa40ee702535e319f492aa2
samiribrh <samiribrahimov2277@gmail.com>
2024-06-29 20:32:50 +0400

    ?!'

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,16 @@
 
 if __name__ == '__main__':
     from sys import argv
-    import MySQLdb
+    import mysql.connector
 
-    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
+    db = mysql.connector.connect(
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
     cursor = db.cursor()
+
     cursor.execute('SELECT * FROM states')
+
     for state in cursor.fetchall():
         print(state)
