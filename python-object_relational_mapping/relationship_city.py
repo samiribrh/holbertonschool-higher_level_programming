d80fa7c34afe93916aa2badd9e768b065af3d7d4
samiribrh <samiribrahimov2277@gmail.com>
2024-06-30 02:17:10 +0400

    Model files

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Module containing improved City class"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""Module Containing the City Class"""
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class City(Base):
-    """The City class"""
-    __tablename__ = 'cities'
+    """The City Class"""
+    __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Module containing improved State class"""
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
+"""Module Containing The State Class"""
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from relationship_city import Base, City
 
 
 class State(Base):
-    """The State class"""
-    __tablename__ = 'states'
-
+    """The State Class"""
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
+
+    cities = relationship("City", backref="state", cascade="all, delete")
