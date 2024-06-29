91a73b136af5df7d0bf5b860818538d667019f8b
samiribrh <samiribrahimov2277@gmail.com>
2024-06-29 22:52:27 +0400

    max 128

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,4 +11,4 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String(128), nullable=False)
