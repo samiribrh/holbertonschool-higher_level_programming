09d781b24f6f03fe0c58c697ee414ef70ba01a9c
samiribrh <samiribrahimov2277@gmail.com>
2024-06-30 00:23:26 +0400

    bored

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module for fetching all states from the database using SQLAlchemy ORM.
+This script connects to a MySQL database
 """
 
 import sys
@@ -8,24 +8,18 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Run only executed
 if __name__ == "__main__":
-    # Defining argv inputs
     usr = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Creating the engine
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(usr, pwd, db_name))
 
-    # Creating the classes
     Base.metadata.create_all(engine)
 
-    # Opening the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Printing the result
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Module containing State class"""
-from sqlalchemy import Column, String, Integer
+"""First state model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    "The State class"""
+    """Represent a state"""
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
