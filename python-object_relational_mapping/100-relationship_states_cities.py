fe5841b2532a95ff3e003e8c62780b3bef27314a
samiribrh <samiribrahimov2277@gmail.com>
2024-06-30 01:57:45 +0400

    state to state_id

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     # Adding new city and state
     new_state = State(name="California")
-    new_city = City(name="San Francisco", state=new_state)
+    new_city = City(name="San Francisco", state_id=new_state.id)
     session.add(new_state)
     session.add(new_city)
 
