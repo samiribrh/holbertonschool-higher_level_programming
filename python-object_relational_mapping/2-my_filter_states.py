6547d140a34c32c4a8f0269a5eee035808c4f4d1
samiribrh <samiribrahimov2277@gmail.com>
2024-06-29 22:04:42 +0400

    Quotation marks

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE BINARY 'name' = '{}' ORDER BY id".format(argv[4]))
 
     for state in cursor.fetchall():
         print(state)
