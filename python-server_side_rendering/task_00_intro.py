8a8912e04962a15becfab82bfb92c496ec5e79ce
samiribrh <samiribrahimov2277@gmail.com>
2024-07-16 15:55:01 +0400

    task_00

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,36 @@
+"""Module containing python script for Jinja2 module"""
+from os.path import exists
+
+
+def generate_invitations(template, attendees_list):
+    """Function for generating invitations"""
+
+    if not template:
+        print("ERROR: template cannot be empty")
+        return
+
+    if not attendees_list:
+        print("ERROR: attendees_list cannot be empty")
+        return
+
+    if not isinstance(template, str):
+        print("ERROR: template must be a string")
+        return
+
+    if (not isinstance(attendees_list, list) or
+            not all(isinstance(item, dict) for item in attendees_list)):
+        print("ERROR: attendees_list must be a list of dictionaries")
+        return
+
+    for index, attendee in enumerate(attendees_list, start=1):
+        template_schema = template
+        for key, value in attendee.items():
+            placeholder = "{" + f"{key}" + "}"
+            if not value:
+                value = f"{key}:N/A"
+            template_schema = template_schema.replace(placeholder, value)
+        if not exists(f"output_{index}.txt"):
+            with open(f"output_{index}.txt", "w") as file:
+                file.write(template_schema)
+        else:
+            print("ERROR: File already exists")
