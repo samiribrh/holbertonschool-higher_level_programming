8f416095b2aa4945eb09eeeba83fba0850fba36f
samiribrh <samiribrahimov2277@gmail.com>
2024-07-28 15:57:26 +0400

    change when clicked

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* task 1
+  JavaScript script that updates the text color
+  of the `header` element to red (`#FF0000`)
+  when the user clicks on the tag.
+*/
+
+const headColor = document.querySelector('header');
+
+const setColor = document.querySelector('#red_header');
+setColor.addEventListener('click', () => {
+  headColor.style.color = '#FF0000';
+});
