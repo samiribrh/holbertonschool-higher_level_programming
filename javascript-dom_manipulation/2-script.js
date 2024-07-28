e1e56b02b7567121f88c5ef6806767e1adead65a
samiribrh <samiribrahimov2277@gmail.com>
2024-07-28 15:58:25 +0400

    add class when user clicks tag

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-/* JavaScript script that updates the text color
-  of the `header` element to red (`#FF0000`)
-  when the user clicks on the tag.
+/* JavaScript script that adds the class `red`
+  to the `header` element when the user clicks on
+  the tag with id `red_header`
 */
 
 const headColor = document.querySelector('header');
 
 const setColor = document.querySelector('#red_header');
 setColor.addEventListener('click', () => {
-  headColor.style.color = '#FF0000';
+  headColor.classList.add('red');
 });
